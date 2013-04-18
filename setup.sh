7d5a73f21a86c8aa12e4ad7f4cbb5902ba7a68d0
nabewata <channel.momo@gmail.com>
2013-04-18 23:47:42 +0900

    シンボリックリンク作成用ファイルを追加

---
+++ setup.sh
@@ -0,0 +1,46 @@
+#!/bin/bash 
+
+# NagasawaTsubasa
+# 2011/05/29
+# Don't move this file
+
+vimdir="vim"
+vimrc="vimrc"
+screenrc="screenrc"
+tmuxconf="tmux.conf"
+zshrc="zshrc"
+gitconfig="gitconfig"
+dircolors="dir_colors"
+
+# The function to make symbolic link
+# Arguments
+# $1 : target of symlink path
+# $2 : linked file path
+function makeSymLink() {
+  if [ -e "$1" ]; then
+    echo "$1 is already exists"
+  else
+    case "$0" in
+      /*) cur=`dirname "$0"` ;;
+      *) cur=`dirname "$PWD/$0"` ;;
+    esac
+    ln -s $cur$2 $1
+    if [ $? -eq 0 ]; then
+      echo "create symlink $1"
+    fi
+  fi
+}
+
+if [ $# -gt 0 ] ;then
+  dir=$1
+else
+  dir="${HOME}/"
+fi
+
+makeSymLink "${dir}.${vimdir}" ""
+makeSymLink "${dir}.${vimrc}" "/${vimrc}"
+makeSymLink "${dir}.${screenrc}" "/${screenrc}"
+makeSymLink "${dir}.${tmuxconf}" "/${tmuxconf}"
+makeSymLink "${dir}.${zshrc}" "/${zshrc}"
+makeSymLink "${dir}.${dircolors}" "/${dircolors}"
+makeSymLink "${dir}.${gitconfig}" "/${gitconfig}"
